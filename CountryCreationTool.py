3ffc4c2c6e19acae94108b7b19a32ba416a7e350
A student <156209522+WGN-CN@users.noreply.github.com>
2024-08-31 08:40:31 +0200

    修复了bug
    修复了bug

--- CountryCreationTool.py
+++ CountryCreationTool.py
@@ -19,6 +19,7 @@ def create_folders_and_files(country_name, country_tag):
 '''
 
 graphical_culture = 
+
 #如graphical_culture = middle_eastern_gfx
 #中东middle_eastern_gfx
 #东欧eastern_european_gfx
@@ -26,7 +27,9 @@ graphical_culture =
 #亚洲asian_gfx
 #非洲african_gfx
 #南美洲和中美洲southamerican_gfx
+
 graphical_culture_2d = 
+
 #如graphical_culture = middle_eastern_2d
 #中东middle_eastern_2d  
 #东欧eastern_european_2d
@@ -35,49 +38,51 @@ graphical_culture_2d =
 #非洲african_2d
 #南美洲和中美洲southamerican_2d
 
-color = { R  G  B }
+color = { 1  1  1 }
 #这个不是国家地块颜色，作者也不知道这里的color是做什么用，建议使用和国家地块颜色相同的RGB代码{国家地块颜色在common\\countries\\colors.txt改}'''
         )
 
     country_tag_file_path = os.path.join(common_dir, "country_tags", f"{country_tag}_请将内容复制到common-country-tags-00_countries.txt.txt")
     with open(country_tag_file_path, "w", encoding="utf-8") as f:
-        f.write(f'{country_tag} = "countries/{country_name}.txt"  “请将内容复制到common-country-tags-00_countries.txt”')
+        f.write(f'{country_tag} = "countries/{country_name}.txt"')
 
     history_file_path = os.path.join(history_dir, "countries", f"{country_tag} - {country_name}.txt")
     with open(history_file_path, "w", encoding="utf-8") as f:
         f.write(
 '''capital = 
-#首都地块province(带#的均为注释，无效果，若想要效果，就删除#)
+#首都地块states(带#的均为注释，无效果，若想要效果，就删除#)
 
-oob =
+#oob =
 #军队数据，若暂时没有，可以在oob前面加上#注释掉 
 
 set_technology = {
+	infantry_weapons = 1
+	mass_assault = 1
 }
 #开始时的科技
-set_stability = 
+set_stability = 0.7
 #设置稳定度，例如70%稳定度，则填入0.7
-set_war_support = 
+set_war_support = 0.7
 #设置战争支持度
 
 recruit_character = 
 #获取领导人，将领（在common\\characters\\你的国家tag）
 
 set_politics = {
-	ruling_party = 
+	ruling_party = neutrality
 	#领导该国家的派系，共产：communism，法西斯：fascism，民主：democratic，中立：neutrality
-	last_election = 
-	#最后一次选举（"年.月.日"  如  "1936.1.1"（p社sb代码，无法早于该日期））
+	last_election = 1936.1.1
+	#最后一次选举（"年.月.日" （p社sb代码，无法早于该日期））
 	election_frequency = 48
 	#选举间隔（48为默认）
 	elections_allowed = 
 	#是否允许公众选举（no或者yes）
 }
 set_popularities = {
-	communism = 
-	fascism =
-	democratic =
-	neutrality =
+	communism = 0
+	fascism = 0
+	democratic = 0
+	neutrality = 100
 	#设置意识形态支持度，总数不超过100，共产：communism，法西斯：fascism，民主：democratic，中立：neutrality
 }
 
@@ -150,16 +155,16 @@ add_ideas = {
 	country = {
 	#不要动	
 
-		国家tag_精神名字，如USA_123(只能英文) = {
-		#国家精神名字
+		{country_tag}_test = {
+		#国家tag_精神名字，如USA_123(只能英文)
 
-			name = 国家tag_精神名字，如USA_123
-			#国家精神名字
+			name = {country_tag}_test 
+			#国家tag_精神名字，如USA_123
 
 			picture = 
             #国家精神图片（p社目前的主流是将其注册在\\interface中的ideas.gfx文件中后，再将其name贴入这里。其构成简单，你们可以自行去那个.gfx文件中学习）
-			allowed = { original_tag =  }
-			#允许哪个国家使用，original_tag = XXX 是原始tag为XXX的国家可以使用该民族精神。想让所有国家都可以使用该民族精神的写法为always = yes
+			allowed = { always = yes }
+			#想要限制哪个国家使用，original_tag = XXX 是只有原始tag为XXX的国家可以使用该民族精神。想让所有国家都可以使用该民族精神的写法为always = yes
 
 			removal_cost = -1
 			#移除花费（一般不动这个）
